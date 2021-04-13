332bc9c5ee549190c8b3cc711537cdb9815bbd4d
Boris <pratnomenis@gmail.com>
2021-04-13 17:55:51 +0300

    third try

--- preload.js
+++ preload.js
@@ -1,27 +1,21 @@
 const {
     ipcRenderer,
-    desktopCapturer,
     nativeImage,
     clipboard
   } = require('electron');
 
+  const path = require('path');
+  const fs = require('fs');
+
   ipcRenderer.on('make-a-shot', async (_, data) => {
-    const { width, height } = data;
-    const sources = await desktopCapturer.getSources({
-        types: ['screen'],
-        thumbnailSize: {
-            width,
-            height
-        }
+    const filePath = path.join('test.txt');
+    fs.readFile(filePath, {
+      encoding: 'utf-8'
+    }, (err, contents) => {
+      const img = nativeImage.createFromDataURL(contents);
+      clipboard.writeImage(img);
+      document.querySelector('h1').innerText = 'image copied to clipboard!'
     });
-    const thumbnail = sources[0].thumbnail;
-    const thumbnail4x = thumbnail.resize({
-      width: width * 4,
-      height: height * 4,
-      quality: 'best'
-    });
-    const imgBase64 = thumbnail4x.toDataURL();
-    const img = nativeImage.createFromDataURL(imgBase64);
-    clipboard.writeImage(img);
-    document.querySelector('h1').innerText = 'image copied to clipboard!'
+
+
   });
